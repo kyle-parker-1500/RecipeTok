113c601e3e3edc1681940cbe336901bb089336fc
Victor Borba <victorfborba9@gmail.com>
2025-11-12 14:14:24 -0800

    Created variables for future use

--- app/src/main/java/com/example/project02group7/MainActivity.java
+++ app/src/main/java/com/example/project02group7/MainActivity.java
@@ -10,6 +10,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainActivity binding;
+    private static final int LOGGED_OUT = -1;
+    // private RECIPETOKRepository repository;
+    // private RECIPETOKViewModel gymLogViewModel;
+    public static final String TAG = "VB_RECIPETOK";
+    private int loggedInUserId = -1;
+    // private User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
