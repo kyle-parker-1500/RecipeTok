3b28834e627372972b8a3b73e585260cae6c4b6d
Kyle Parker <kylerparker1500@gmail.com>
2025-12-01 23:16:51 -0800

    believe i've finished copying/modifying code from alejandro's branch

--- app/src/main/java/com/example/project02group7/MainActivity.java
+++ app/src/main/java/com/example/project02group7/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.project02group7.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -21,6 +22,45 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView2);
+        // Find bottom nav bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Create instances of existing fragments
+        Fragment home = new HomeFragment();
+        Fragment recipes = new RecipeFragment();
+        Fragment account = new AccountFragment();
+        Fragment settings = new SettingsFragment();
+
+        // set initial fragment
+        setCurrentFragment(home);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        // handle bottom nav user selection
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.home) {
+                setCurrentFragment(home);
+            } else if (id == R.id.recipe) {
+                setCurrentFragment(recipes);
+            } else if (id == R.id.profile) {
+                setCurrentFragment(account);
+            } else if (id == R.id.setting) {
+                setCurrentFragment(settings);
+            }
+            return true;
+        });
     }
+
+    /**
+     * Description: Method to help switch between fragments.
+     * @param fragment a Fragment object
+     */
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.mainFragment, fragment)
+                .commit();
+    }
+
 }
---
+++ app/src/main/java/com/example/project02group7/SettingsFragment.java
@@ -0,0 +1,20 @@
+package com.example.project02group7;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // inflate layout
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
